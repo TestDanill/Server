c83b5d498db1cdf8b024a12c9caef2946fcea496
Daniil1380 <Tkachenko1380@yandex.ru>
2020-08-21 16:54:56 +0300

    сделал что-то

--- src/Server.java
+++ src/Server.java
@@ -4,8 +4,7 @@ import java.net.Socket;
 import java.util.HashMap;
 
 public class Server {
-    public static void main(String[] args) throws IOException {
-    }
+
 }
 
 //создать сервер, который получает имя города и по нему пишет какая температура сейчас (подсказка: лучше
